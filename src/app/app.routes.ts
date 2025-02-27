0a2849949a1c32abdcae3f6306537bf6f8ec546f
Strikerzzzz <strikezz2003@gmail.com>
2025-02-27 13:48:35 +0700

    thêm route cho học tập

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,5 +8,6 @@ export const routes: Routes = [
   { path: 'email-confirmation', loadChildren: () => import('./email-confirmation/email-confirmation.routes').then(m => EMAIL_CONFIRMATION_ROUTES) },
   { path: 'admin', loadChildren: () => import('./pages/admin/admin.routes').then(m => m.ADMIN_ROUTES), canActivate: [AuthGuard], data: { role: 'Admin' } },
   { path: 'lecturer', loadChildren: () => import('./pages/lecturer/lecturer.routes').then(m => m.LECTURER_ROUTES), canActivate: [AuthGuard], data: { role: 'Lecturer' } },
-  { path: 'learning', loadChildren: () => import('./pages/learning/learning.routes').then(m => m.LEARNING_ROUTES) }
+  { path: 'learning', loadChildren: () => import('./pages/learning/learning.routes').then(m => m.LEARNING_ROUTES) },
+  { path: 'my-learning', loadChildren: () => import('./pages/my-learning/my-learning.routes').then(m => m.MYLEARNING_ROUTES), canActivate: [AuthGuard] }
 ];
---
+++ src/app/pages/my-learning/my-learning.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from "@angular/router";
+import { MyLearningComponent } from "./my-learning.component";
+export const MYLEARNING_ROUTES: Routes = [
+    { path: '', component: MyLearningComponent }
+];
